ec73d081cd9037bf13038f010d62d48a4dbee808
saiidhanna21 <hanna.saiid2003@gmail.com>
2023-05-09 17:13:41 +0300

    Initialized

---
+++ index.js
@@ -0,0 +1,44 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
+
+const express = require('express');
+const session = require('express-session');
+const flash = require('express-flash');
+const mysql = require('mysql');
+const methodOverride = require('method-override');
+const bodyParser = require('body-parser');
+const ExpressError = require('./ExpressError');
+const app = express();
+
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(bodyParser.json());
+app.use(flash());
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+}))
+
+
+app.get('/', (req, res) => {
+    res.send("Good Job!");
+})
+
+app.all("*", (req, res, next) => {
+    next(new ExpressError("Page Not Found", 400));
+})
+
+//create a new repository in github with the name of garage and connect it to this ?
+
+app.use((err, req, res, next) => {
+    const { status = 500 } = err;
+    if (!err.message) err.message = "Oh No, Something Went Wrong!";
+    res.status(status).render('error', { err });
+});
+
+const port = 5000;
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+})
